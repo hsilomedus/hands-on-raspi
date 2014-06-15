00db6b0c5c852a80b1f48984ffe035929702d0be
hsilomedus <pancec@yahoo.com>
2014-06-15 19:49:20 +0200

    gpio sample

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
 
 public class Main {
-  public static void main(String []args) {
-    System.out.println("Hello world again!");
+  public static void main(String []args) throws InterruptedException {
+	  GpioController gpio = GpioFactory.getInstance();
+	  
+	  System.out.println("Pin going UP!");
+	  GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(
+			  RaspiPin.GPIO_00,
+			  "LED",
+			  PinState.HIGH);
+	  Thread.sleep(5000);
+	  System.out.println("Ping going down.");
+	  pin.low();
+	  gpio.shutdown();
   }
 }
