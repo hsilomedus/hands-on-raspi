c57848ec967361690555c0c55d3ac4dd605f7ec7
hsilomedus <pancec@yahoo.com>
2014-06-20 01:15:39 +0200

    run script param

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 echo ----------------------------------------------------------------------
+set JAVAFX_DEBUG=1
+export JAVAFX_DEBUG=1
 sudo java -cp mantarget:lib/pi4j-core.jar Main
 echo ----------------------------------------------------------------------
 
