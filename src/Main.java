5befb88854afc1db7384c5a5899469a58b336902
hsilomedus <pancec@yahoo.com>
2014-06-20 00:47:22 +0200

    initial FX

--- src/Main.java
+++ src/Main.java
@@ -7,45 +7,94 @@ import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class Main {
-	static boolean change = false;
+public class Main extends Application {
+	
+	@Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
 
-	public static void main(String[] args) {
-		GpioController gpio = GpioFactory.getInstance();
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
 
-		System.out.println("Starting up!");
-		final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(
-				RaspiPin.GPIO_00, "LED", PinState.LOW);
-		final GpioPinDigitalInput pinInput = gpio.provisionDigitalInputPin(
-				RaspiPin.GPIO_01, PinPullResistance.PULL_DOWN);
-		pinInput.addListener(new GpioPinListenerDigital() {
-			@Override
-			public void handleGpioPinDigitalStateChangeEvent(
-					GpioPinDigitalStateChangeEvent event) {
-				if (event.getState().equals(PinState.HIGH)) {
-					System.out.println("Taster clicked!");
-					change = !change;
-				}
-			}
+        Button btnExit = new Button();
+        btnExit.setText("Exit");
+        btnExit.setOnAction(new EventHandler<ActionEvent>() {
 
-		});
+            @Override
+            public void handle(ActionEvent event) {
+                Platform.exit();
+            }
+        });
 
-		System.out.println("Click on the taster to start/stop blinking!");
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(btn, btnExit);
 
-		try {
-			while (true) {
-				Thread.sleep(200);
+        StackPane root = new StackPane();
+        root.getChildren().add(vBox);
 
-				if (change) {
-					pin.toggle();
-				}
-			}
-		} catch (InterruptedException e) {
+        Scene scene = new Scene(root, 300, 250);
 
-		} finally {
-			System.out.println("Finish!");
-			gpio.shutdown();
-		}
-	}
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+	
+	
+//	static boolean change = false;
+//	public static void main(String[] args) {
+//		GpioController gpio = GpioFactory.getInstance();
+//
+//		System.out.println("Starting up!");
+//		final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(
+//				RaspiPin.GPIO_00, "LED", PinState.LOW);
+//		final GpioPinDigitalInput pinInput = gpio.provisionDigitalInputPin(
+//				RaspiPin.GPIO_01, PinPullResistance.PULL_DOWN);
+//		pinInput.addListener(new GpioPinListenerDigital() {
+//			@Override
+//			public void handleGpioPinDigitalStateChangeEvent(
+//					GpioPinDigitalStateChangeEvent event) {
+//				if (event.getState().equals(PinState.HIGH)) {
+//					System.out.println("Taster clicked!");
+//					change = !change;
+//				}
+//			}
+//
+//		});
+//
+//		System.out.println("Click on the taster to start/stop blinking!");
+//
+//		try {
+//			while (true) {
+//				Thread.sleep(200);
+//
+//				if (change) {
+//					pin.toggle();
+//				}
+//			}
+//		} catch (InterruptedException e) {
+//
+//		} finally {
+//			System.out.println("Finish!");
+//			gpio.shutdown();
+//		}
+//	}
 }
